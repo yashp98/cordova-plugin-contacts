4542892f3e7b1328a8bcc0e93c1f42db6476bf10
Inês Sousa <ines.sousa@outsystems.com>
2019-08-07 11:29:50 +0100

    test: update wdio.android.app.config.js (device name is now "Android")
    https://outsystemsrd.atlassian.net/browse/RNMT-3066

--- os-appium-tests/config/wdio.android.app.conf.js
+++ os-appium-tests/config/wdio.android.app.conf.js
@@ -10,12 +10,12 @@ config.capabilities = [
         // The defaults you need to have in your config
         automationName: 'UiAutomator2',
         platformName: 'Android',
-        deviceName: 'Nexus',
+        deviceName: 'Android',
         app: join(process.cwd(), 'apps/Contacts_Sample_App.apk'), // Path to your native app
         // appPackage: 'com.outsystemsenterprise.enmobiletst.ContactsSampleApp', // Path to your app package
         // appActivity: 'com.outsystemsenterprise.enmobiletst.ContactsSampleApp.MainActivity', // Path to your activity
         // chromedriver windows:
-        chromedriver: join(process.cwd(), 'chromedriver/chromedriver.exe'),
+        chromedriver: join(process.cwd(), 'chromedriver/chromedriver_75.exe'),
         // chromedriver iOS:
         // chromedriver: join(process.cwd(), 'chromedriver/chromedriver_76_iOS'),
         noReset: true
