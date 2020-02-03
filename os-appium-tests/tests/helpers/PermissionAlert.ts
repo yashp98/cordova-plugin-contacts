e3a176b37692f62454b69150d9a4d11e6fe07dd0
Inês Sousa <ines.sousa@outsystems.com>
2020-02-03 10:29:07 +0000

    test: change permission alert in android

--- os-appium-tests/tests/helpers/PermissionAlert.ts
+++ os-appium-tests/tests/helpers/PermissionAlert.ts
@@ -1,16 +1,20 @@
 import {DEFAULT_TIMEOUT, DEFAULT_TIMEOUT_INTERVAL} from '../constants';
+import * as AndroidUtils from '../helpers/AndroidUtils';
+import * as IosUtils from '../helpers/IOSUtils';
 
 const SELECTORS = {
     ANDROID: {
-        PERMISSION_DIALOG : '*//android.widget.LinearLayout[@resource-id="com.android.packageinstaller:id/dialog_container"]',
-        PERMISSION_ALLOW_BUTTON : '*//android.widget.Button[@resource-id="com.android.packageinstaller:id/permission_allow_button"]',
-        PERMISSION_DENY_BUTTON: '*//android.widget.Button[@resource-id="com.android.packageinstaller:id/permission_deny_button"]'    
+        // PERMISSION_DIALOG : '*//android.widget.LinearLayout[@resource-id="com.android.packageinstaller:id/dialog_container"]',
+        // PERMISSION_DIALOG : '*//android.widget.LinearLayout[@resource-id="com.google.android.permissioncontroller"]',
+        // PERMISSION_ALLOW_BUTTON : '*//android.widget.Button[@resource-id="com.android.packageinstaller:id/permission_allow_button"]',
+        PERMISSION_ALLOW_BUTTON : 'id/permission_allow_button',
+        PERMISSION_DENY_BUTTON: '*//android.widget.Button[@resource-id="com.android.packageinstaller:id/permission_deny_button"]',
     },
 
     IOS: {
         PERMISSION_DIALOG: '*//XCUIElementTypeAlert',
         PERMISSION_ALLOW_BUTTON: '*//XCUIElementTypeButton[@name="OK"]',
-        PERMISSION_DENY_BUTTON: '*//XCUIElementTypeButton[@name="Don’t Allow"]'
+        PERMISSION_DENY_BUTTON: '*//XCUIElementTypeButton[@name="Don’t Allow"]',
     },
 };
 
@@ -20,31 +24,44 @@ class PermissionAlert {
      * Wait for the alert to exist
      */
     public static waitForIsShown(isShown = true, driver): void {
-        const selector = driver.isAndroid ? SELECTORS.ANDROID.PERMISSION_DIALOG : SELECTORS.IOS.PERMISSION_DIALOG;
-        $(selector).waitForExist(DEFAULT_TIMEOUT, !isShown); 
+        this.getPermissionButton(true, driver).waitForExist(DEFAULT_TIMEOUT, !isShown);
     }
 
     /**
      * Check if exists the alert
      */
-    public static isShown(isShown = true, driver): boolean {
-        const selector = driver.isAndroid ? SELECTORS.ANDROID.PERMISSION_DIALOG : SELECTORS.IOS.PERMISSION_DIALOG;
-        return $(selector).isDisplayed();
+    public static isShown(driver): boolean {
+        const permissionButton = this.getPermissionButton(true, driver);
+        if (permissionButton === undefined) {
+            return false;
+        } else {
+            return permissionButton.isDisplayed();
+        }
     }
 
- 
     /**
      * Allow or deny a permission request
      * 
-     * @param {boolean} allow 
+     * @param {boolean} allow
      */
-    public static allowPermission(allow = true, driver): void {        
-        const selectors = driver.isAndroid ? SELECTORS.ANDROID : SELECTORS.IOS;
-        const buttonSelector = allow ? selectors.PERMISSION_ALLOW_BUTTON : selectors.PERMISSION_DENY_BUTTON;
-        const permissionButton = $(buttonSelector);
-        permissionButton.click();
+    // public static allowPermission(allow = true, driver): void {
+    //     const selectors = driver.isAndroid ? SELECTORS.ANDROID : SELECTORS.IOS;
+    //     const buttonSelector = allow ? selectors.PERMISSION_ALLOW_BUTTON : selectors.PERMISSION_DENY_BUTTON;
+    //     const permissionButton = $(buttonSelector);
+    //     permissionButton.click();
+    // }
+
+    public static allowPermission(allow = true, driver): void {
+       this.getPermissionButton(allow, driver).click();
     }
 
+    public static getPermissionButton(allow = true, driver): WebdriverIO.Element {
+        const selectors = driver.isAndroid ? SELECTORS.ANDROID : SELECTORS.IOS;
+        const buttonSelector = allow ? selectors.PERMISSION_ALLOW_BUTTON : selectors.PERMISSION_DENY_BUTTON;
+        return driver.isAndroid ?
+            AndroidUtils.getElemByPartialId(buttonSelector, false) :
+            IosUtils.getElemByXPath(buttonSelector, false);
+    }
 
     /**
      * Get the alert text
